afaa6a81515613202335dd1e0130699e66d3c813
Matt Aimonetti <mattaimonetti@gmail.com>
2014-10-01 20:02:51 -0700

    updated the documentation

---
+++ README.md
@@ -0,0 +1,8 @@
+#Static Server
+
+This project is just a very simple server that you can run to serve
+static content.
+
+It was created to help Stephanie learn HTML and CSS/JS. 
+Browsers won't let you load local static JS so you need a server like
+this one.
--- main.go
+++ main.go
@@ -27,8 +27,12 @@ func main() {
 		}
 	}
 	if _, err := os.Stat(staticPath); os.IsNotExist(err) {
-		log.Fatalf("This server is trying to serve static content from: \n%s\nbut this folder doesn't exist, please create it or pass a 'path' argument when starting the server.", staticPath)
+		fmt.Printf("This server is trying to serve static content from: \n%s\nbut this folder doesn't exist, please create it or pass a 'path' argument when starting the server.", staticPath)
+		fmt.Printf("To create a folder from the command line, type: mkdir static\n")
 	}
 	log.Printf("About to start the server on port %d\nServing content from %s", *port, staticPath)
+	log.Println("Open the following address in your browser:")
+	log.Println("Put your files inside the static folder and they will available via")
+	log.Printf("http://localhost:%d\n", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), http.FileServer(http.Dir(staticPath))))
 }
